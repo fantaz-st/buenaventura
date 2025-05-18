376cfcd328fe45b0ead98f2d43bf887a0c9f062e
fantaz-st <cbabic@pfst.hr>
2025-05-18 13:45:49 +0200

    AnimatedText fix, added single tour page

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,12 +7,11 @@ import "./globals.css";
 import "@fancyapps/ui/dist/fancybox/fancybox.css";
 import Header from "@/Components/Header/Header";
 import Footer from "@/Components/Footer/Footer";
-import HamburgerMenu from "@/Components/HamburgerMenu/HamburgerMenu";
 
 const inter = Inter({ subsets: ["latin"], style: "normal", variable: "--font-inter" });
 
 export const metadata = {
-  title: "Rebelde Boats",
+  title: "Rebelde Boats | Split Island-Hopping Day Trips & Exclusive Charters",
   description: "Private boat tours in Split, Croatia. Rebelde boats offers luxury island-hopping experiences.",
   keywords: ["Rebelde", "Boat Tours Split", "Croatia Private Tour", "Island Hopping", "Hvar", "Blue Cave", "Blue lagoon"],
 };
@@ -34,7 +33,6 @@ export default function RootLayout({ children }) {
           {children}
           <Analytics />
           <Footer />
-          {/* <HamburgerMenu /> */}
         </ReactLenis>
       </body>
     </html>
---
+++ src/app/our-tours/three-islands-tour/page.js
@@ -0,0 +1,7 @@
+export default function ThreeIslandsTour() {
+  return (
+    <main>
+      <h1>hi!</h1>
+    </main>
+  );
+}
