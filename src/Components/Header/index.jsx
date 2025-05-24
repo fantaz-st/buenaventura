a91b2f60c507b0bce494b8427ef591c5e815af35
fantaz-st <cbabic@pfst.hr>
2025-05-24 15:31:04 +0200

    header fix

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,14 +1,8 @@
 import dynamic from "next/dynamic";
 import HeaderStatic from "./HeaderStatic";
 
-/**
- * Entry point used by pages/layouts.
- * – Streams <HeaderStatic/> from the server.
- * – Swaps in <HeaderHydrated/> on the client *after* JS bundles load.
- */
 const HeaderHydrated = dynamic(() => import("./HeaderHydrated"), {
-  ssr: false,
-  loading: () => <HeaderStatic />, // static placeholder during JS download
+  loading: () => <HeaderStatic />,
 });
 
 export default function Header() {
