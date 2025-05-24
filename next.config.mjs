339d71225e0fe13948bc09ac765e3565d761a4ee
fantaz-st <cbabic@pfst.hr>
2025-05-24 15:58:55 +0200

    removed vercel.json and reverted config

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/_vercel/insights/script.js",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
