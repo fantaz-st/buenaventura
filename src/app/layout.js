a0409feb43afdc6e69592bc476c7e5db87869000
fantaz-st <cbabic@pfst.hr>
2025-07-18 22:10:28 +0200

    removed vercel analytics, added googles

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,8 @@ import Footer from "@/Components/Footer/Footer";
 import { Inter } from "next/font/google";
 
 import { ReactLenis } from "lenis/react";
-import { Analytics } from "@vercel/analytics/next";
+import Script from "next/script";
+
 import "./reset.css";
 import "./globals.css";
 import "@fancyapps/ui/dist/fancybox/fancybox.css";
@@ -21,16 +22,41 @@ export const viewport = {
 };
 
 export default function RootLayout({ children }) {
+  const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
+
   return (
     <html lang="en">
       <head>
         <link rel="manifest" href="/site.webmanifest" />
+
+        {/* GTM script */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}');
+            `,
+          }}
+        />
       </head>
       <body className={inter.variable}>
+        {/* GTM no‑script fallback */}
+        <noscript>
+          <iframe src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`} height="0" width="0" style={{ display: "none", visibility: "hidden" }} />
+        </noscript>
+
         <Header />
         <ReactLenis root>
           {children}
-          <Analytics />
           <Footer />
         </ReactLenis>
       </body>
